e81c5b03e6a9a89de1f2d3d9e3453913002bef6b
Zachary Nado <znado@google.com>
2021-08-09 18:11:10 -0700

    Open sourcing Diabetic Retinopathy Detection experiment configs.
    PiperOrigin-RevId: 389769312

--- baselines/diabetic_retinopathy_detection/experiments/dropout_tune.py
+++ baselines/diabetic_retinopathy_detection/experiments/dropout_tune.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-r"""Deterministic baseline for Diabetic Retinopathy Detection.
+r"""Dropout baseline for Diabetic Retinopathy Detection.
 
 """
 
@@ -23,7 +23,6 @@ import os.path
 
 from ml_collections import config_dict
 
-
 launch_on_gcp = True
 
 
@@ -31,23 +30,25 @@ def get_config():
   """Returns the configuration for this experiment."""
   config = config_dict.ConfigDict()
   config.user = getpass.getuser()
-  config.priority = 'prod'
-  config.platform = 'gpu'
-  config.gpu_type = 't4'
-  config.num_gpus = 1
+  config.gfs_user = config.user + '-brain'
+  config.cell = 'iz'
+  config.priority = 'freebie'
+  config.platform = 'df'
+  config.tpu_topology = '2x2'
   config.experiment_name = (
       os.path.splitext(os.path.basename(__file__))[0] + '_' +
       datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S'))
-  output_dir = 'gs://launcher-beta-test-bucket/{}'.format(
+  output_dir = 'gs://launcher-beta-test-bucket/diabetic_retinopathy_detection/{}'.format(
       config.experiment_name)
+  data_dir = 'gs://ub-data/retinopathy'
   config.args = {
       'train_epochs': 90,
+      'use_gpu': False,  # Use TPU.
       'train_batch_size': 64,
       'eval_batch_size': 64,
-      'checkpoint_interval': -1,
-      'lr_schedule': 'step',
       'output_dir': output_dir,
-      'data_dir': 'gs://ub-data/retinopathy',
+      'checkpoint_interval': -1,
+      'data_dir': data_dir,
   }
   return config
 
@@ -58,4 +59,5 @@ def get_sweep(hyper):
       hyper.loguniform('base_learning_rate', hyper.interval(1e-3, 0.1)),
       hyper.loguniform('one_minus_momentum', hyper.interval(1e-2, 0.1)),
       hyper.loguniform('l2', hyper.interval(1e-5, 1e-3)),
+      hyper.uniform('dropout_rate', hyper.interval(0.01, 0.25)),
   ], length=num_trials)
